a200181e95df1b0be0959b7ca1070365d4b5a55c
gregorbee <gregor.belc@gmail.com>
2025-12-04 14:37:41 +0100

    Update script01.js

--- script01.js
+++ script01.js
@@ -1,3 +1,4 @@
-var ProjehtId = document.getElementById('project_id').value;
-getDeleiverablesByProjectIdandGateId(ProjehtId,'2','2');
-window.alert(ProjehtId);
+window.alert('TEST');
+// var ProjehtId = document.getElementById('project_id').value;
+// getDeleiverablesByProjectIdandGateId(ProjehtId,'2','2');
+// window.alert(ProjehtId);
