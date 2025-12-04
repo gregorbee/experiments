21e5655562b3ede213a0130cf6712943c11fc75b
gregorbee <gregor.belc@gmail.com>
2025-12-04 14:49:50 +0100

    Update script01.js

--- script01.js
+++ script01.js
@@ -1,4 +1,17 @@
-window.alert('TEST');
+'use strict';
+
+export const builtinScriptlets = [];
+
+builtinScriptlets.push({
+    name: 'find-project-id.fn',
+    fn: findId,
+});
+function findId() {
+  window.alert('TEST');
+  return document.getElementById('project_id').value;
+}
+
+// window.alert('TEST');
 // var ProjehtId = document.getElementById('project_id').value;
 // getDeleiverablesByProjectIdandGateId(ProjehtId,'2','2');
 // window.alert(ProjehtId);
