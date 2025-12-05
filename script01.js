4cb94e6b8d3f09f0132d08a637feecd25539f808
gregorbee <gregor.belc@gmail.com>
2025-12-05 12:26:43 +0100

    Update script01.js

--- script01.js
+++ script01.js
@@ -4,18 +4,17 @@
 /// world ISOLATED
 // example.com##+js(tmf)
 function testMyFunction() {
-	//const tmf = ( ) => {
-		//try {
-			window.alert('TEST');
-    		//return document.getElementById('project_id').value;
-			//}
-		//} catch { }
-	//};
-	//runAt(( ) => { tmf(); }, 'interactive');
+	 window.alert('TEST');
 }
 /******************************************************************************/
 // window.alert('TEST');
 // var ProjehtId = document.getElementById('project_id').value;
 // getDeleiverablesByProjectIdandGateId(ProjehtId,'2','2');
 // window.alert(ProjehtId);
+	//const tmf = ( ) => {
+		//try {
+		//	  }
+		//} catch { }
+	//};
+	//runAt(( ) => { tmf(); }, 'interactive');
 /******************************************************************************/
