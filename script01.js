0753feec77c33ad8a31873233d622bf77605a637
gregorbee <gregor.belc@gmail.com>
2025-12-05 12:38:21 +0100

    Update script01.js

--- script01.js
+++ script01.js
@@ -3,18 +3,15 @@
 /// alias tmf.js
 /// world ISOLATED
 // example.com##+js(tmf)
+
 function testMyFunction() {
-	 window.alert('TEST');
+	window.alert('This is a test.');
+	document.getElementById("1_deliverableContent").innerHTML="Text change";
 }
+
 /******************************************************************************/
-// window.alert('TEST');
+// window.alert('This is a test');
 // var ProjehtId = document.getElementById('project_id').value;
 // getDeleiverablesByProjectIdandGateId(ProjehtId,'2','2');
 // window.alert(ProjehtId);
-	//const tmf = ( ) => {
-		//try {
-		//	  }
-		//} catch { }
-	//};
-	//runAt(( ) => { tmf(); }, 'interactive');
 /******************************************************************************/
