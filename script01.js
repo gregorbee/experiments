42143037440b312d8e13687e1791cf5bececdd44
gregorbee <gregor.belc@gmail.com>
2025-12-05 07:57:44 +0100

    Update script01.js

--- script01.js
+++ script01.js
@@ -5,14 +5,34 @@ export const builtinScriptlets = [];
 /******************************************************************************/
 
 builtinScriptlets.push({
+    name: 'myTest.js',
+    requiresTrust: true,
+    fn: trustedTest,
+    world: 'ISOLATED',
+    //dependencies: [
+        //'run-at-html-element.fn',
+        //'safe-self.fn',
+    //],
+});
+function trustedTest(
+    selectors = '',
+    extraMatch = '',
+    delay = ''
+) {
+    if ( extraMatch !== '' ) { return; }
+}
+
+/******************************************************************************/
+
+builtinScriptlets.push({
     name: 'find-project-id.js',
     requiresTrust: true,
     fn: trustedFindId,
     world: 'ISOLATED',
 });
 function trustedFindId() {
-  window.alert('TEST');
-  // return document.getElementById('project_id').value;
+    window.alert('TEST');
+    //return document.getElementById('project_id').value;
 }
 
 /******************************************************************************/
